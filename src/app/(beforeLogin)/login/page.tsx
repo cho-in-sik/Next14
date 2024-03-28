e5772682e35d3b8045e2b860b4a53a2005992d57
조인식 <imsif42@naver.com>
2024-03-28 17:24:23 +0900

    Feat: 폴더 구조 변경, css 적용

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,15 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Main from '@/app/(beforeLogin)/_component/Main';
 
 export default function Login() {
-  redirect('/i/flow/login');
+  const router = useRouter();
+  router.replace('/i/flow/login');
+  return <Main />;
 }
+
+//router.push 와 router.replace의 차이는 뒤로가기를 했을때 발생.
+//router.replace 는 바로 뒤가 아니라 전의 전 주소로 이동.
+
+//만약 우리가 이 프로젝트에서 push 를 써버리면 local/login 으로 가는데 여기로 가봤자 다시 i/flow/login으로 이동하기에 벗어날 수가 없다.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,6 @@ export const metadata: Metadata = {
 type Props = {
   children: React.ReactNode;
 };
-
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
