49efccae7e530c8c14f083f4f3f36d934c855fd0
조인식 <imsif42@naver.com>
2024-04-20 22:06:01 +0900

    feat: test

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,6 +1,7 @@
 import Main from '@/app/(beforeLogin)/_component/Main';
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
+///ds
 
 export default async function Home() {
   const session = await auth();
