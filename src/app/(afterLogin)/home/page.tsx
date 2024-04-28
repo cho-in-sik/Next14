a05ef3aa91bb53e9625ce688a35aba1b255da147
조인식 <imsif42@naver.com>
2024-04-28 16:02:16 +0900

    chore: react-query 추가

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -3,8 +3,10 @@ import Tab from '@/app/(afterLogin)/home/_component/Tab';
 import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider';
 import PostForm from '@/app/(afterLogin)/home/_component/PostForm';
 import Post from '../_component/Post';
+import { QueryClient } from '@tanstack/react-query';
 
 export default function Home() {
+  const queryClient = new QueryClient();
   return (
     <main className={style.main}>
       <TabProvider>
